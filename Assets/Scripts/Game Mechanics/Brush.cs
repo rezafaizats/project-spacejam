f7ea4e21be9147b7870470b86b87d7cb65ecc332
Arya Rakha <arrakhpra@gmail.com>
2025-06-09 17:02:23 +0200

    brushing fixed

--- Assets/Scripts/Game Mechanics/Brush.cs
+++ Assets/Scripts/Game Mechanics/Brush.cs
@@ -7,7 +7,8 @@ public class Brush : MonoBehaviour
     [SerializeField] private Texture2D dirtyMaskBase;
     [SerializeField] private Texture2D brushTexture;
     [SerializeField] private Material objectMaterial;
-
+    [SerializeField] private float brushScale = 1f;
+    
     private Texture2D dirtyMask;
     private bool isBrushActive;
     private float dirtAmountTotal;
@@ -48,30 +49,43 @@ public class Brush : MonoBehaviour
                 // Debug.Log("UV : " + textureCoord + " Pixels : " + paintPixelPosition);
                 int paintPixelDistance = Mathf.Abs(paintPixelPosition.x - lastPaintPixelPosition.x) + Mathf.Abs(paintPixelPosition.y - lastPaintPixelPosition.y);
                 int maxPaintDistance = 7;
+                if (paintPixelDistance < maxPaintDistance) {
+                    // Painting too close to last position
+                    return;
+                }
+                
                 lastPaintPixelPosition = paintPixelPosition;
 
-                int pixelXOffset = pixelX - (brushTexture.width / 2);
-                int pixelYOffset = pixelY - (brushTexture.height / 2);
+                int scaledBrushWidth = Mathf.RoundToInt(brushTexture.width * brushScale);
+                int scaledBrushHeight = Mathf.RoundToInt(brushTexture.height * brushScale);
+                int pixelXOffset = Mathf.Clamp(pixelX - scaledBrushWidth / 2, 0, dirtyMask.width);
+                int pixelYOffset = Mathf.Clamp(pixelY - scaledBrushHeight / 2, 0, dirtyMask.height);
 
-                for (int x = 0; x < brushTexture.width; x++)
-                {
-                    for (int y = 0; y < brushTexture.height; y++)
-                    {
-                        Color pixelDirt = brushTexture.GetPixel(x, y);
-                        Color pixelDirtMask = dirtyMask.GetPixel(pixelXOffset + x, pixelYOffset + y);
+                int clampedWidth = Mathf.Min(scaledBrushWidth, dirtyMask.width - pixelXOffset);
+                int clampedHeight = Mathf.Min(scaledBrushHeight, dirtyMask.height - pixelYOffset);
+
+                if (clampedWidth <= 0 || clampedHeight <= 0)
+                    return;
+
+                Color[] maskPixels = dirtyMask.GetPixels(pixelXOffset, pixelYOffset, clampedWidth, clampedHeight);
+
+                for (int x = 0; x < clampedWidth; x++) {
+                    for (int y = 0; y < clampedHeight; y++) {
+                        float u = x / (float)clampedWidth;
+                        float v = y / (float)clampedHeight;
+
+                        Color pixelDirt = brushTexture.GetPixelBilinear(u, v);
+                        int index = y * clampedWidth + x;
+                        Color pixelDirtMask = maskPixels[index];
 
                         float removedAmount = pixelDirtMask.g - (pixelDirtMask.g * pixelDirt.g);
                         dirtAmount -= removedAmount;
 
-                        dirtyMask.SetPixel(
-                            pixelXOffset + x,
-                            pixelYOffset + y,
-                            new Color(0, pixelDirtMask.g * pixelDirt.g, 0)
-                        );
-                        Debug.Log("Dirt amount : " + dirtAmount);
+                        maskPixels[index] = new Color(0, pixelDirtMask.g * pixelDirt.g, 0);
                     }
                 }
 
+                dirtyMask.SetPixels(pixelXOffset, pixelYOffset, clampedWidth, clampedHeight, maskPixels);
                 dirtyMask.Apply();
                 Debug.Log("Dirt amount : " + dirtAmount);
             }
---
+++ Assets/Scripts/Game Mechanics/RocksController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Game_Mechanics
+{
+    public class RocksController : MonoBehaviour
+    {
+        [SerializeField] private MeshFilter meshReference;
+        [SerializeField] private Transform rocksParent;
+        [SerializeField] private GameObject[] rockPrefabs;
+        [SerializeField] private Vector3 scaleMin, scaleMax;
+        [SerializeField] private int generateCount = 20;
+        
+        private void Start()
+        {
+            GenerateRocks();
+        }
+
+        private void GenerateRocks()
+        {
+            var verts = meshReference.sharedMesh.vertices;
+            var normals = meshReference.sharedMesh.normals;
+
+            var indexes = Enumerable.Range(0, verts.Length).OrderBy(_ => Random.value).Take(generateCount);
+
+            foreach (var i in indexes)
+            {
+                var randPrefab = rockPrefabs[Random.Range(0, rockPrefabs.Length)];
+                var rock = Instantiate(randPrefab);
+                rock.transform.position = GetPosition(verts[i], meshReference.transform.localScale);
+                rock.transform.forward = normals[i];
+                rock.transform.SetParent(rocksParent, false);
+                rock.transform.localScale = new Vector3(
+                    Random.Range(scaleMin.x, scaleMax.x),
+                    Random.Range(scaleMin.y, scaleMax.y),
+                    Random.Range(scaleMin.z, scaleMax.z)
+                );
+            }
+        }
+
+        private Vector3 GetPosition(Vector3 verticePos, Vector3 localScale)
+        {
+            return new Vector3(
+                    verticePos.x * localScale.x,
+                    verticePos.y * localScale.y,
+                    verticePos.z * localScale.z
+            );
+        }
+    }
+}
