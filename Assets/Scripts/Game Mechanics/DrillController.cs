b209807eb78785e73944a80d99be8c9fadddf334
Arya Rakha <arrakhpra@gmail.com>
2025-06-14 18:06:29 +0200

    change to fbx

--- Assets/Scripts/Game Mechanics/DrillController.cs
+++ Assets/Scripts/Game Mechanics/DrillController.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Game_Mechanics
 {
@@ -10,6 +12,7 @@ namespace Game_Mechanics
         [SerializeField] private float damageMultiplier = 1f;
         [SerializeField] private GameObject drillObject;
         [SerializeField] private GameObject drillCursor;
+        [SerializeField] private GameObject drillCursorVisual;
 
         private bool isActive;
         private Camera cam;
@@ -31,26 +34,61 @@ namespace Game_Mechanics
         {
             if (!isActive) return;
 
-            bool hasRock = TryGetRock(out var rock);
+            bool hasRock = TryGetRockNew(out var rock, out var hit);
             drillCursor.SetActive(hasRock);
 
             if (!hasRock) return;
+
+            drillCursor.transform.position = hit.point;
             
             if (!Input.GetMouseButton(0)) return;
             rock.Damage(Time.deltaTime * damageMultiplier);
+            drillCursorVisual.transform.localPosition = Random.insideUnitCircle * 0.01f;
 
         }
 
-        private bool TryGetRock(out Rock rock)
+        private bool TryGetRockNew(out Rock rock, out RaycastHit hit)
         {
             rock = null;
-            
-            var hits = Physics.RaycastAll(cam.ScreenPointToRay(Input.mousePosition));
+            hit = default;
 
-            if (hits.Length == 0) return false;
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (!Physics.Raycast(ray, out hit)) return false;
+            return hit.transform.TryGetComponent(out rock);
+        }
 
-            foreach (var hit in hits)
-                if (!hit.collider.TryGetComponent(out rock)) return true;
+        private bool TryGetRock(out Rock rock, out RaycastHit raycastHit)
+        {
+            rock = null;
+            raycastHit = default;
+
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = new RaycastHit[8];
+            int size = Physics.RaycastNonAlloc(ray, hits);
+
+            float closestSqrDistance = float.MaxValue;
+            RaycastHit closestHit = default;
+            Rock foundRock = null;
+
+            for (int i = 0; i < size; i++)
+            {
+                var hit = hits[i];
+                float sqrDistance = (ray.origin - hit.point).sqrMagnitude;
+
+                if (sqrDistance < closestSqrDistance && hit.collider.TryGetComponent(out Rock tempRock))
+                {
+                    closestSqrDistance = sqrDistance;
+                    closestHit = hit;
+                    foundRock = tempRock;
+                }
+            }
+
+            if (foundRock != null)
+            {
+                rock = foundRock;
+                raycastHit = closestHit;
+                return true;
+            }
 
             return false;
         }
