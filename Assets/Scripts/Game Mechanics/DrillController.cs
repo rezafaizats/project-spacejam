f844bf78d071787259b9b246891175ba7d51df6b
Arya Rakha <arrakhpra@gmail.com>
2025-06-14 17:33:48 +0200

    ...

--- Assets/Scripts/Game Mechanics/DrillController.cs
+++ Assets/Scripts/Game Mechanics/DrillController.cs
@@ -9,6 +9,7 @@ namespace Game_Mechanics
         
         [SerializeField] private float damageMultiplier = 1f;
         [SerializeField] private GameObject drillObject;
+        [SerializeField] private GameObject drillCursor;
 
         private bool isActive;
         private Camera cam;
@@ -29,19 +30,29 @@ namespace Game_Mechanics
         private void Update()
         {
             if (!isActive) return;
-            if (!Input.GetMouseButton(0)) return;
 
+            bool hasRock = TryGetRock(out var rock);
+            drillCursor.SetActive(hasRock);
+
+            if (!hasRock) return;
+            
+            if (!Input.GetMouseButton(0)) return;
+            rock.Damage(Time.deltaTime * damageMultiplier);
+
+        }
+
+        private bool TryGetRock(out Rock rock)
+        {
+            rock = null;
+            
             var hits = Physics.RaycastAll(cam.ScreenPointToRay(Input.mousePosition));
-            if (hits.Length == 0) return;
+
+            if (hits.Length == 0) return false;
 
             foreach (var hit in hits)
-            {
-                Debug.Log($"Hitting {hit.collider.name}");
-                if (!hit.collider.TryGetComponent<Rock>(out var rock)) continue;
-            
-                rock.Damage(Time.deltaTime * damageMultiplier);
-                break;
-            }
+                if (!hit.collider.TryGetComponent(out rock)) return true;
+
+            return false;
         }
     }
 }
